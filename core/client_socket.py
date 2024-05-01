5e53e7d989ce039a329cb966e0880c21b772a4a0
ditto <ditto@Ditto.myguest.virtualbox.org>
2024-05-01 09:02:45 +0530

    core with client and server class

---
+++ core/client_socket.py
@@ -0,0 +1,43 @@
+import socket as s
+import pickle as pk
+import time
+
+
+class ClientSocket:
+    """Create Client Socket"""
+    def __init__(self):
+        self.server_sock = s.socket(s.AF_INET, s.SOCK_STREAM)
+
+    
+    def connect_server(self, server_ip, port=None):
+        """Connect to server"""
+        try:
+            self.server_sock.connect((server_ip, port))
+            print("connected with server")
+        except ConnectionRefusedError:
+            print('connection refused error')
+
+    def send_data(self, data):
+        """Function to serilize and send data through socket"""
+        picked_data = pk.dumps(data)
+        self.server_sock.send(picked_data)
+
+    def check_reachable(self):
+        """Check server reachability"""
+        try:
+            retry=0
+            while True:
+                if self.server_sock.sendall(pk.dumps("server reach check")) is None:
+                    return True
+                retry+=1
+                if retry == 5:
+                    break
+                print("Failed to reach server, retrying again in few seconds")
+                time.sleep(5)
+        except OSError:
+            print('os error')
+            return False
+    
+    def close_sock(self):
+        """Close server connection"""
+        self.server_sock.close()
