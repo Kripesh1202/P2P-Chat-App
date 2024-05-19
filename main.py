9d1803cc171f3e2a5e7a5093bc75b47135538278
kripesh1202 <kripesh1202@gmail.com>
2024-05-19 13:18:09 +0530

    updated error handling

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-import src.server as s
-import src.client as c
-import threading
+"""Main file to start chatapp"""
 
-def user_input():
-    client_sock, ip_addr = serv.create_server()
-    server_sock = clie.connect_server('127.0.0.1', 1201)
+from chatapp import frame
 
-    while True:
-        user_input = str(input("Enter your msg: "))
-
-        client_sock.send_data(user_input)
-
-
+chatapp = frame.ChatFrame()
+chatapp.init_frame()
