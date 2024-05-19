acd96a14aab1636882b28bc83fc5cf6dcb324571
kripesh1202 <kripesh1202@gmail.com>
2024-05-19 16:09:37 +0530

    umerged main

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-"""Main file to start chatapp"""
+import src.server as s
+import src.client as c
+import threading
 
-from chatapp import frame
+def user_input():
+    client_sock, ip_addr = serv.create_server()
+    server_sock = clie.connect_server('127.0.0.1', 1201)
 
-chatapp = frame.ChatFrame()
-chatapp.init_frame()
+    while True:
+        user_input = str(input("Enter your msg: "))
+
+        client_sock.send_data(user_input)
+
+
